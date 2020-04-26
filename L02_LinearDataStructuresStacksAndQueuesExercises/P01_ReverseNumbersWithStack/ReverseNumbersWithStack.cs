178c0a7221822468c6d2170b0b6168e321490cc3
Dimitar Balabanov <dimitar.balabanov@gmail.com>
2020-04-26 22:20:45 +0300

    Added first two exercises from L02_LinearDataStructuresStacksAndQueuesExercises

---
+++ L02_LinearDataStructuresStacksAndQueuesExercises/P01_ReverseNumbersWithStack/ReverseNumbersWithStack.cs
@@ -0,0 +1,30 @@
+﻿namespace P01_ReverseNumbersWithStack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ReverseNumbersWithStack
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var stack = new Stack<int>();
+            foreach (var num in numbers)
+            {
+                stack.Push(num);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = stack.Pop();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ L02_LinearDataStructuresStacksAndQueuesExercises/P02_CalculateSequenceWithQueue/CalculateSequenceWithQueue.cs
@@ -0,0 +1,27 @@
+﻿namespace P02_CalculateSequenceWithQueue
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class CalculateSequenceWithQueue
+    {
+        public static void Main()
+        {
+            var input = int.Parse(Console.ReadLine());
+            var queue = new Queue<int>();
+            queue.Enqueue(input);
+
+            var result = new int[50];
+            for (int i = 0; i < 50; i++)
+            {
+                var current = queue.Dequeue();
+                result[i] = current;
+                queue.Enqueue(current + 1);
+                queue.Enqueue(2 * current + 1);
+                queue.Enqueue(current + 2);
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}

6773863e6a8742d2b97a642c7bb61adb4bbb5664
Dimitar Balabanov <dimitar.balabanov@gmail.com>
2020-04-28 19:44:00 +0300

    Added P04_LinkedStack from L02_LinearDataStructuresStacksAndQueuesExercises

---
+++ L02_LinearDataStructuresStacksAndQueuesExercises/P04_LinkedStack/LinkedStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace P04_LinkedStack
+{
+    public class LinkedStack<T>
+    {
+        private Node firstNode;
+
+        public int Count { get; private set; }
+
+        public void Push(T element) 
+        {
+            this.firstNode = new Node(element, this.firstNode);
+            this.Count++;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var element = this.firstNode.value;
+            this.firstNode = this.firstNode.NextNode;
+            this.Count--;
+            return element;
+        }
+
+        public T[] ToArray() 
+        {
+            var arr = new T[this.Count];
+            var current = this.firstNode;
+            var index = 0;
+
+            while (current != null)
+            {
+                arr[index++] = current.value;
+                current = current.NextNode;
+            }
+
+            return arr;
+        }
+
+        private class Node
+        {
+            public T value;
+
+            public Node(T value, Node nextNode = null)
+            {
+                this.value = value;
+                this.NextNode = nextNode;
+            }
+
+            public Node NextNode { get; set; }
+        }
+    }
+}
---
+++ L02_LinearDataStructuresStacksAndQueuesExercises/P04_LinkedStack/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P04_LinkedStack
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var stack = new LinkedStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+            stack.Push(7);
+            Console.WriteLine(string.Join(" ", stack.ToArray()));
+        }
+    }
+}

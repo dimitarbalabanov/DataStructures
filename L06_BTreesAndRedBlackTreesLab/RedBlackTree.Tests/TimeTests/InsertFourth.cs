af250106d358a350d53240368518676eea680a47
Dimitar Balabanov <dimitar.balabanov@gmail.com>
2020-05-20 16:21:27 +0300

    Added L06_BTreesAndRedBlackTreesLab

---
+++ L06_BTreesAndRedBlackTreesLab/RedBlackTree.Tests/TimeTests/InsertFourth.cs
@@ -0,0 +1,20 @@
+﻿namespace RedBlackTree.Tests.TimeTests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class InsertFourth
+    {
+        [Test]
+        [Timeout(500)]
+        public void Insert_MultipleElements_ShouldBeFast()
+        {
+            RedBlackTree<int> rbt = new RedBlackTree<int>();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                rbt.Insert(i);
+            }
+        }
+    }
+}

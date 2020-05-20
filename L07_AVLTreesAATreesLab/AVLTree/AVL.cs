2bbcb9aae9a52433c4c775198a573505c6271d92
Dimitar Balabanov <dimitar.balabanov@gmail.com>
2020-05-20 17:39:47 +0300

    Added L07_AVLTreesAATreesLab

---
+++ L07_AVLTreesAATreesLab/AVLTree/AVL.cs
@@ -0,0 +1,169 @@
+﻿namespace AVLTree
+{
+    using System;
+
+    public class AVL<T> where T : IComparable<T>
+    {
+        private Node root;
+
+        public Node Root
+        {
+            get
+            {
+                return this.root;
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            var node = this.Search(this.root, item);
+            return node != null;
+        }
+
+        public void Insert(T item)
+        {
+            this.root = this.Insert(this.root, item);
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            this.EachInOrder(this.root, action);
+        }
+
+        private Node Insert(Node node, T item)
+        {
+            if (node == null)
+            {
+                return new Node(item);
+            }
+
+            int cmp = item.CompareTo(node.Value);
+            if (cmp < 0)
+            {
+                node.Left = this.Insert(node.Left, item);
+            }
+            else if (cmp > 0)
+            {
+                node.Right = this.Insert(node.Right, item);
+            }
+
+            node = Balance(node);
+            UpdateHeight(node);
+
+            return node;
+        }
+
+        private Node Balance(Node node)
+        {
+            var balance = Height(node.Left) - Height(node.Right);
+            if (balance > 1)
+            {
+                var childBalance = Height(node.Left.Left) - Height(node.Left.Right);
+                if (childBalance < 0)
+                {
+                    node.Left = RotateLeft(node.Left);
+                }
+
+                node = RotateRight(node);
+            }
+            else if (balance < -1)
+            {
+                var childBalance = Height(node.Right.Left) - Height(node.Right.Right);
+                if (childBalance > 0)
+                {
+                    node.Right = RotateRight(node.Right);
+                }
+
+                node = RotateLeft(node);
+            }
+
+            return node;
+        }
+
+        private void UpdateHeight(Node node)
+        {
+            node.Height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
+        }
+
+        private int Height(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return node.Height;
+        }
+
+        private Node RotateRight(Node node)
+        {
+            var left = node.Left;
+            node.Left = left.Right;
+            left.Right = node;
+
+            UpdateHeight(node);
+
+            return left;
+        }
+
+        private Node RotateLeft(Node node)
+        {
+            var right = node.Right;
+            node.Right = right.Left;
+            right.Left = node;
+
+            UpdateHeight(node);
+
+            return right;
+        }
+
+        private Node Search(Node node, T item)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            int cmp = item.CompareTo(node.Value);
+            if (cmp < 0)
+            {
+                return Search(node.Left, item);
+            }
+            else if (cmp > 0)
+            {
+                return Search(node.Right, item);
+            }
+
+            return node;
+        }
+
+        private void EachInOrder(Node node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.EachInOrder(node.Left, action);
+            action(node.Value);
+            this.EachInOrder(node.Right, action);
+        }
+
+        public class Node
+        {
+            public T Value;
+
+            public Node Left;
+
+            public Node Right;
+
+            public int Height;
+
+            public Node(T value)
+            {
+                this.Value = value;
+                this.Height = 1;
+            }
+        }
+    }
+}
